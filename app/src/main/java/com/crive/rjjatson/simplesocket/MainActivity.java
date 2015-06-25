757e6710bd4266ad45c0bc23398b82138451285d
Thoriq Satriya <thoriq.satriya94@gmail.com>
2015-06-25 13:46:41 +0700

    test

--- app/src/main/java/com/crive/rjjatson/simplesocket/MainActivity.java
+++ app/src/main/java/com/crive/rjjatson/simplesocket/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+		//test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         outText = (TextView)findViewById(R.id.textView2);
